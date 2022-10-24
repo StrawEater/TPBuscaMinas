3a0700f851bcdf386a705e64e08cf96ae3423f8d
clinux01 <juani8.bustos@gmail.com>
2022-10-24 11:16:01 -0300

    initial commit

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #include "auxiliares.h"
 
 int main(int argc, char **argv) {
-    std::cout << "Implementando TPI!!" << std::endl;
+    std::cout << "Implementando TPI! !" << std::endl;
     testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();;
