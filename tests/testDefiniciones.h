6d7cd487b775f8ae3527c832a8a780a1d099711e
SirNando <matiasnandin@hotmail.com>
2022-10-28 11:38:50 -0300

    Updated testDefiniciones.h/aunNoGano

--- tests/testDefiniciones.h
+++ tests/testDefiniciones.h
@@ -52,11 +52,11 @@ static jugadas jugadasGano = {
 };
 
 static jugadas aunNoGano = {
-        jugada(pos(2, 0), 2),
-        jugada(pos(4, 0), 1),jugada(pos(0, 1), 3),jugada(pos(1, 1), 3),jugada(pos(2, 1), 3),
-        jugada(pos(4, 1), 1),jugada(pos(0, 2), 1),jugada(pos(2, 2), 2),jugada(pos(3, 2), 1),
-        jugada(pos(4, 2), 1),jugada(pos(0, 3), 2),jugada(pos(1, 3), 3),jugada(pos(2, 3), 2),jugada(pos(3, 3), 1),
-        jugada(pos(4, 3), 0),jugada(pos(1, 4), 2),jugada(pos(3, 4), 1),jugada(pos(4, 4), 0),
+        jugada(pos(0, 2), 2),
+        jugada(pos(0, 4), 1),jugada(pos(1, 0), 3),jugada(pos(1, 1), 3),jugada(pos(1, 2), 3),
+        jugada(pos(1, 4), 1),jugada(pos(2, 0), 1),jugada(pos(2, 2), 2),jugada(pos(2, 3), 1),
+        jugada(pos(2, 4), 1),jugada(pos(3, 0), 2),jugada(pos(3, 1), 3),jugada(pos(3, 2), 2),jugada(pos(3, 3), 1),
+        jugada(pos(3, 4), 0),jugada(pos(4, 1), 2),jugada(pos(4, 3), 1),jugada(pos(4, 4), 0),
 };
 
 static jugadas noGano = {
